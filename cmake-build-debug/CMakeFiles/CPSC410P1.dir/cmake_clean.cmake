2f59ab231fbff605842c5986f18ff3b909827b86
Allan Pedin <allan.pedin.17@cnu.edu>
2020-02-01 18:53:15 -0500

    initial commit

---
+++ cmake-build-debug/CMakeFiles/CPSC410P1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CPSC410P1.dir/main.cpp.o"
+  "CPSC410P1"
+  "CPSC410P1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CPSC410P1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
